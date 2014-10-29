2d2c22434364bbcce5f0026521b2309f32642345
david.beal@akretion.com <david.beal@akretion.com>
2014-10-29 17:55:39 +0100

    [ADD] module mrp_stock_location

---
+++ mrp_stock_location/__openerp__.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) All Rights Reserved 2014 Akretion
+#    @author David BEAL <david.beal@akretion.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+
+{
+    'name': 'MRP Stock Location',
+    'version': '0.1',
+    'author': 'Akretion',
+    'maintener': 'Akretion',
+    'category': 'Manufaturing',
+    'summary': "Allow to use alternative locations",
+    'depends': [
+        'mrp',
+    ],
+    'description': """
+MRP Stock Location
+======================
+
+
+
+Contributors
+------------
+* David BEAL <david.beal@akretion.com>
+
+""",
+    'website': 'http://www.akretion.com/',
+    'data': [
+    ],
+    'demo': [
+        'demo/res.partner.csv',
+        'demo/company.xml',
+        'demo/product.xml',
+    ],
+    'external_dependencies': {
+        'python': [],
+    },
+    'license': 'AGPL-3',
+    'tests': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ mrp_stock_location/stock.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#  licence AGPL version 3 or later
+#  see licence in __openerp__.py or http://www.gnu.org/licenses/agpl-3.0.txt
+#  Copyright (C) 2014 Akretion (http://www.akretion.com).
+#  @author David BEAL <david.beal@akretion.com>
+#
+##############################################################################
+
+from openerp.osv import orm
+
+
+class MrpProduction(orm.Model):
+    _inherit = 'mrp.production'
+
+    def default_get(self, cr, uid, fields, context=None):
+        res = super(MrpProduction, self).default_get(
+            cr, uid, fields, context=context)
+        # We only needs to change location field values which are in res keys
+        location_fields = set(res.keys()).intersection(
+            ['location_src_id', 'location_dest_id'])
+        for field in location_fields:
+            res[field] = self._change_location(
+                cr, uid, field, res[field], context=None)
+        return res
+
+    def _change_location(self, cr, uid, loc_field, location, context=None):
+        """ You may inherit this method to change
+            from/to location according to loc_field
+            super(...)
+            if loc_field == 'location_src_id':
+                return X
+            else:
+                return Y
+        """
+        return location
