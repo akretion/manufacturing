6ecdaf7e54b40be9e27ce8ba775dfb0ea10f7efb
david.beal@akretion.com <david.beal@akretion.com>
2014-12-24 10:47:51 +0100

    [FIX] rename module to mrp_workcenter_load

--- mrp_workcenter_load/wizard/compute.py
+++ mrp_workcenter_load/wizard/compute.py
@@ -10,6 +10,7 @@
 
 from openerp.osv import orm
 import time
+from openerp.tools.misc import (DEFAULT_SERVER_DATETIME_FORMAT as ERP_DATETIME)
 
 
 class HierarchicalWorkcenterLoad(orm.TransientModel):
@@ -21,9 +22,9 @@ class HierarchicalWorkcenterLoad(orm.TransientModel):
         workcenters_clause = ", ".join([str(x) for x in workcenter_ids])
         return (states_clause, workcenters_clause)
 
-    def _sql_query(self, cr, uid, context=None):
+    def _get_sql_query(self, cr, uid, context=None):
         query = """
-            SELECT wl.workcenter_id AS id, sum(wl.hour) AS hour_load
+            SELECT wl.workcenter_id AS id, sum(wl.hour) AS hour
             FROM mrp_production_workcenter_line wl
                 LEFT JOIN mrp_production mp ON wl.production_id = mp.id
             WHERE mp.state IN (%s) and wl.workcenter_id IN (%s)
@@ -31,6 +32,17 @@ class HierarchicalWorkcenterLoad(orm.TransientModel):
         """ % (self._add_sql_clauses(cr, uid, self._workcter_ids,
                                      context=context))
         return query
+
+    def _write_load(self, cr, uid, result, context=None):
+        workcenter_hours = {}
+        for elm in result:
+            workcenter_hours[elm['id']] = elm['hour']
+            vals = {'load': elm['hour'],
+                    'global_load': elm['hour'],
+                    'last_compute': time.strftime(ERP_DATETIME)}
+            self.pool['mrp.workcenter'].write(
+                cr, uid, [elm['id']], vals, context=context)
+        return workcenter_hours
 
     def compute_load(self, cr, uid, ids, context=None):
         self._workcter_ids = self.pool['mrp.workcenter'].search(
@@ -40,23 +52,23 @@ class HierarchicalWorkcenterLoad(orm.TransientModel):
         self.pool['mrp.workcenter'].write(cr, uid, self._workcter_ids, vals,
                                           context=context)
         # Compute time for workcenters in mrp_production_workcenter_line
-        cr.execute(self._sql_query(cr, uid, context=context))
-        res = cr.dictfetchall()
-        workcenter_hours = {}
-        for elm in res:
-            workcenter_hours[elm['id']] = elm['hour_load']
-            vals = {'load': elm['hour_load'],
-                    'global_load': elm['hour_load'],
-                    'last_compute': time.strftime('%Y-%m-%d %H:%M:%S')}
-            self.pool['mrp.workcenter'].write(
-                cr, uid, [elm['id']], vals, context=context)
+        cr.execute(self._get_sql_query(cr, uid, context=context))
+        result = cr.dictfetchall()
+        workcenter_hours = self._write_load(cr, uid, result, context=context)
         # Compute upper level data
         self._aggregate_values(
             cr, uid, workcenter_hours, context=context)
-        return True
+        return {
+            'name': 'Workcenters Load',
+            'view_type': 'tree',
+            'view_mode': 'tree',
+            'res_model': 'mrp.workcenter',
+            'type': 'ir.actions.act_window',
+            'target': 'current',
+        }
 
     def _aggregate_values(self, cr, uid, work_hours, context=None):
-        res = self._build_hierarchical_list(cr, uid, context=context)
+        res = self._build_hierarchyical_list(cr, uid, context=context)
         print res
         for elm in res:
             parent, children = elm.items()[0]
@@ -64,15 +76,15 @@ class HierarchicalWorkcenterLoad(orm.TransientModel):
                                  for child in children])
             work_hours[parent] = children_time + work_hours.get(parent, 0)
             vals = {'global_load': work_hours[parent],
-                    'last_compute': time.strftime('%Y-%m-%d %H:%M:%S')}
+                    'last_compute': time.strftime(ERP_DATETIME)}
             self.pool['mrp.workcenter'].write(
                 cr, uid, parent, vals, context=context)
 
-    def _build_hierarchical_list(self, cr, uid, context=None):
+    def _build_hierarchyical_list(self, cr, uid, context=None):
         """ return a workcenter relations list from LOW level to HIGH level
         [{parent_id1: [child_id1]}, {parent_id2: [child_id5, child_id7]}, ...]
         """
-        hierarch, filtered_hierarchy = [], []
+        hierarchy, filtered_hierarchy = [], []
         pos_in_list = {}
         position = 0
         query = """
@@ -84,16 +96,15 @@ class HierarchicalWorkcenterLoad(orm.TransientModel):
         res = cr.dictfetchall()
         # we assign nodes in the right order to make aggregation reliable
         for elm in res:
-            hierarch.append({elm['id']: []})
+            hierarchy.append({elm['id']: []})
             pos_in_list[elm['id']] = position
             position += 1
         for elm in res:
-            # None values (without parent_id) are not used
             if elm['parent']:
-                value = hierarch[pos_in_list[elm['parent']]][elm['parent']]
+                value = hierarchy[pos_in_list[elm['parent']]][elm['parent']]
                 value.append(elm['id'])
-                hierarch[pos_in_list[elm['parent']]][elm['parent']] = value
-        for elm in hierarch:
+                hierarchy[pos_in_list[elm['parent']]][elm['parent']] = value
+        for elm in hierarchy:
             parent, children = elm.items()[0]
             if children:
                 # only nodes with children are useful
